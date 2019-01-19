8b4cb1374bb75bc82f7a1f4a05bb62c53df8b225
Som3Dude <46487879+Som3Dude@users.noreply.github.com>
2019-01-19 20:10:03 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,3 @@
-function 1Player() {
-  var Detectives = document.getElementById("detectives");
-  var Traitors = document.getElementById("traitors");
-  var Innocents = document.getElementById("innocents");
-  Traitors.innerHTML = "Traitors: Error";
+function PlayerOne() {
+  document.getElementById("traitors").innerHTML = "Traitors: Error";
 }
