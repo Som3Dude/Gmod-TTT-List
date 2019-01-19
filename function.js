1f015f5958989db7f5689fcf21809c7d5cf48f7a
Som3Dude <46487879+Som3Dude@users.noreply.github.com>
2019-01-19 19:59:10 +0100

    creation of new stuff

---
+++ function.js
@@ -0,0 +1,7 @@
+var Detectives = document.getelementById("detectives");
+var Traitors = document.getelementById("traitors");
+var Innocents = document.getelementById("innocents");
+
+function 1Player() {
+  Traitors.innerHTML = "Traitors: Error";
+}
