299be5c0bcf5b432bb13d95919d664e73c0fe6bd
Som3Dude <46487879+Som3Dude@users.noreply.github.com>
2019-01-21 21:24:41 +0100

    Update layerdatawork1.js

--- layer1/layerdatawork1.js
+++ layer1/layerdatawork1.js
@@ -3,6 +3,8 @@ function SendName() {
 	Name.elements[0].value;
 	if(Name.elements[0].value == "Holy") {
 		document.getElementById("greeting").style.display = "none";
+		document.getElementById("main_screen").style.display = "block";
+		document.getElementById("Welcome_Message").innerHTML = "Welcome "+Name.elements[0].value;
 	}
 	else {
 		document.getElementById("denied").style.display = "block";
