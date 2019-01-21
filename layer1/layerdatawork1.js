ee35965f9444b81897360ba86fc919490f0a459a
Som3Dude <46487879+Som3Dude@users.noreply.github.com>
2019-01-21 20:44:30 +0100

    Update layerdatawork1.js

--- layer1/layerdatawork1.js
+++ layer1/layerdatawork1.js
@@ -1,7 +1,7 @@
-function SemdName() {
+function SendName() {
 	var Name = document.getElementById("NameForm");
 	Name.elements[0].value;
-	if(NameInput.elements[0].value == "Holy") {
+	if(Input.elements[0].value == "Holy") {
 		document.getElementById("greeting").style.display = "none";
 	}
 	else {
