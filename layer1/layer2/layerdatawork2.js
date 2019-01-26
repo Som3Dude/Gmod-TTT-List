eeb8030d5e78bf2d248bc1f2101f4a9f1961254b
Som3Dude <46487879+Som3Dude@users.noreply.github.com>
2019-01-26 13:34:12 +0100

    Update layerdatawork2.js

--- layer1/layer2/layerdatawork2.js
+++ layer1/layer2/layerdatawork2.js
@@ -1,6 +1,28 @@
-setInterval( loadopen, 250 );
-function loadopen() {
-	var inner = document.getElementById("loadcircle12");
-	var outer = document.getElementById("loadcircle11");
+function start() {
 	var progress = 0;
+	setInterval( loadopen, 250 );
+	function loadopen() {
+		var inner = document.getElementById("loadcircle12");
+		var outer = document.getElementById("loadcircle11");
+		var message = document.getElementById("loadingmessage");
+		if(progress < 328) {
+			inner.style.width = progress + "px";
+			inner.style.height = progress + "px";
+			progress = progress + 1;
+		}
+		else {
+			inner.style.width = progress + "px";
+			inner.style.height = progress + "px";
+			progress = progress - 1;
+		}
+		if(progress == 20) {
+			message.innerHTML = "Loading.";
+		}
+		if(progress == 150) {
+			message.innerHTML = "Loading..";
+		}
+		if(progress == 300) {
+			message.innerHTML = "Loading...";
+		}
+	}
 }
