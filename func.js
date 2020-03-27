517bbafdad73d22cd8d4c62fea3dd0058f1fe58e
Holy-Person <holyochi@gmail.com>
2020-03-27 20:26:29 +0100

    Added Calculator Interface
    Rough interface for calculator,
    still disabled because i need to figure out how to calculate the roles.

--- func.js
+++ func.js
@@ -4,6 +4,7 @@ function startUp() {
   document.getElementById("MapInnerList").addEventListener('click', openLink);
   document.getElementById("WeaponInnerList").addEventListener('click', openLink);
   document.getElementById("OtherInnerList").addEventListener('click', openLink);
+  updateCalculator();
 }
 
 function openLink() {
@@ -13,4 +14,40 @@ function openLink() {
   if(className != "InnerList") {
     window.open(`steam://openurl/https://steamcommunity.com/sharedfiles/filedetails/?id=`+id);
   }
+}
+
+function updateCalculator() {
+  var playerNumField = document.getElementById(`PlayerNum`);
+  var innoNumField = document.getElementById(`InnoNum`);
+  var traitorNumField = document.getElementById(`TraitorNum`);
+  var detectiveNumField = document.getElementById(`DetecNum`);
+  
+  var ammountPlayers = playerNumField.value;
+  var ammountInnocent = 2;
+  var ammountTraitor = 1;
+  var ammountDetective = 0;
+  if(!isNaN(ammountPlayers)) {
+    if(ammountPlayers < 2) {
+      ammountInnocent = 0;
+      ammountTraitor = 0;
+      ammountDetective = 0;
+      playerNumField.style.color = "#660000";
+    } else {
+      if(ammountPlayers >= 5){ammountDetective = 1;}else{ammountDetective = 0;}
+      if(ammountPlayers%2 != 1) {
+        
+      } else {
+        
+      }
+      playerNumField.style.color = "#000000";
+    }
+  } else {
+    ammountInnocent = 0;
+    ammountTraitor = 0;
+    ammountDetective = 0;
+    playerNumField.style.color = "#660000";
+  }
+  innoNumField.innerHTML = ammountInnocent;
+  traitorNumField.innerHTML = ammountTraitor;
+  detectiveNumField.innerHTML = ammountDetective;
 }
